badc1d42eff702bdc30fd1ffe42619f05879697a
Abhilash-bare1709 <bareabhilash2000@gmail.com>
2022-09-18 15:38:33 +0530

    Encapsulation sample Added

---
+++ Encapsulation.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using std::string;
+
+class EmpSalary{
+
+
+private:
+
+    string Name;
+    int Salary;
+
+public:
+    
+    EmpSalary(string name,int salary){
+        Name=name;
+        Salary=salary;
+    }
+
+    int getname(string name){
+        Name=name;
+    }
+
+    int getsalary(int sal){
+        if(sal>150000)
+        Salary=sal;
+    }
+
+    void display(){
+        std::cout<<"name "<<Name  <<" salary "<<Salary;
+    }
+ };
+
+
+int main(){
+    EmpSalary salary1=EmpSalary("Shamil",200000);
+
+   
+    salary1.getname("abhilash");
+
+    salary1.getsalary(50000);
+
+
+    salary1.display();
+    // salary1.Name("fdsds");
+
+}
